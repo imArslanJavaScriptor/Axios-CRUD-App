73e39ede791801a1d9a89fd1f1523bab719b12a2
Muhammad Arslan <imarslan7061@gmail.com>
2025-01-21 00:34:34 +0500

    init

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function App() {
+  return <div className="w-full min-h-screen flex items-center justify-center bg-blue-500 text-white text-5xl font-extrabold">App</div>;
+}
+
+export default App;
